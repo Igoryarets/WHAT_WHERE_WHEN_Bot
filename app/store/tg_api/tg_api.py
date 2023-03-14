6a587adfd3b358c7c5220bf001393dc5869398c4
Igoryarets <igoryarets338@gmail.com>
2023-03-14 11:24:55 +0300

    work with answer

--- app/store/tg_api/tg_api.py
+++ app/store/tg_api/tg_api.py
@@ -19,7 +19,9 @@ class TgClient:
             async with session.get(url) as resp:
                 return await resp.json()
 
-    async def get_updates(self, offset: Optional[int] = None, timeout: int = 0) -> dict:
+    async def get_updates(self,
+                          offset: Optional[int] = None,
+                          timeout: int = 0) -> dict:
         url = self.get_url("getUpdates")
         params = {}
         if offset:
@@ -30,9 +32,11 @@ class TgClient:
             async with session.get(url, params=params) as resp:
                 return await resp.json()
 
-    async def get_updates_in_objects(self, offset: Optional[int] = None, timeout: int = 0) -> GetUpdatesResponse:
+    async def get_updates_in_objects(self,
+                                     offset: Optional[int] = None,
+                                     timeout: int = 0) -> GetUpdatesResponse:
         res_dict = await self.get_updates(offset=offset, timeout=timeout)
-        logging.info(f'dict updates {res_dict}')       
+        logging.info(f'dict updates {res_dict}')
         return GetUpdatesResponse.Schema().load(res_dict)
 
     async def send_message(
@@ -40,12 +44,10 @@ class TgClient:
                             chat_id: Optional[int] = None,
                             text: Optional[str] = None,
                             keyboard: Optional[dict] = None,
-                            message_id: Optional[int] = None) -> SendMessageResponse:
-        
-        logging.info('work send message')
+                            ) -> SendMessageResponse:
 
+        logging.info('work send message')
         url = self.get_url("sendMessage")
-        
         if keyboard:
             payload = {
                 'chat_id': chat_id,
@@ -56,12 +58,10 @@ class TgClient:
             payload = {
                 'chat_id': chat_id,
                 'text': text,
-            } 
-
+            }
 
         async with aiohttp.ClientSession() as session:
             async with session.post(url, json=payload) as resp:
                 res_dict = await resp.json()
                 logging.info(f'send message {res_dict}')
-                return SendMessageResponse.Schema().load(res_dict)        
-  
+                return SendMessageResponse.Schema().load(res_dict)
